fbb0d1bf5e88f7816421b4dd9619e48f59cd2dcc
Radoslaw Smigielski <radoslaw.smigielski@gmail.com>
2018-08-06 14:31:39 +0200

    Added metadata to display on PyP

--- setup.py
+++ setup.py
@@ -8,4 +8,16 @@ setup(
     packages=find_packages(),
     scripts=['py-pastebin.py'],
     install_requires=['requests'],
+
+    author="Radoslaw Smigielski",
+    author_email="",
+    description="py-pastebin cmd tool to create new pastes on pastebin.com",
+    license="GPL",
+    keywords="paste pastebin",
+    url="https://github.com/radeksm/py-pastebin",
+    project_urls={
+        "Bug Tracker": "https://github.com/radeksm/py-pastebin/issues",
+        "Documentation": "https://github.com/radeksm/py-pastebin/blob/master/README.md",
+        "Source Code": "https://github.com/radeksm/py-pastebin",
+    }
 )
